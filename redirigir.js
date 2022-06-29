13e3e21a7a7fd0c425b33efdcf894fa4c9a29e66
lmardilav <65024856+lmardilav@users.noreply.github.com>
2022-06-29 14:02:17 -0500

    Add files via upload

---
+++ redirigir.js
@@ -0,0 +1,12 @@
+// let body = document.body;
+// setTimeout( function redigir(){
+//     location.href="https://deals.getkailo.io/P1PGBKJ/214CW98/";
+// }, 1000*3);
+
+
+// Redirigir a otra pagina sin que se muestre la pagina original.
+function redirigir (){
+    let link="https://deals.getcarbonklean.io/P1PGBKJ/TNJXHC/?uid=161";
+    window.location.replace (link);
+};
+redirigir();
